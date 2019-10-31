42df296c80cfb97cd2653dab5a4c83088601488f
Danilo Alves <daniloha7@gmail.com>
2019-10-31 10:49:24 -0300

    [feature/api-get-name] API para retornar nome

---
+++ server.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+from flask import Flask, render_template, request, jsonify
+import connexion
+
+Flask(__name__, template_folder="templates")
+app = connexion.App(__name__,specification_dir='./')
+app.add_api('swagger.yml')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ services.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+
+def get_name(user):
+    msg = f"Hello, {user}"
+    return msg
+    
